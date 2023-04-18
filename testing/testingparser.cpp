4b3fc4813b1c6fe6f1fccab21251386f0ec2c4b4
Gerald Freislich <freislichg@gmail.com>
2023-04-18 16:16:38 +0930

    safe

--- testing/testingparser.cpp
+++ testing/testingparser.cpp
@@ -9,7 +9,9 @@ int main()
     std::string line;
     std::cin >> line;
     std::vector<std::string> commands = parser(&line);
-    
+    for(auto a: commands){
+        std::cout << a << "\n";
+    }
 }
 
 /*
@@ -18,5 +20,16 @@ Returns vector
 */
 std::vector<std::string> parser(std::string *str)
 {
-
+    std::vector<std::string> commandies;
+    std::string substr;
+    for(int i = 0; i < str->size(); i++)
+    {
+        
+        if(str->at(i) != ' ')
+        {
+            substr += str->at(i);
+        } else {
+            
+        }
+    }
 }
