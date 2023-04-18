0dd63988f2082b39902e6b3591433e05c87a6dd5
Gerald Freislich <freislichg@gmail.com>
2023-04-18 16:51:30 +0930

    safe

--- main.cpp
+++ main.cpp
@@ -2,21 +2,65 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> parser(std::string *);
+std::vector<std::string> parser(std::string);
+void executer(std::vector<std::string>);
 
 int main()
 {
     std::string line;
-    std::cin >> line;
-    std::vector<std::string> commands = parser(&line);
-    
+    std::getline(std::cin, line);
+    std::vector<std::string> commands = parser(line);
+    for(auto command: commands) std::cout << command << "\n";
+    executer(commands);
 }
 
-/*
-Takes a string filled with spaces and stores the parts into a vector
-Returns vector
-*/
-std::vector<std::string> parser(std::string *str)
+std::vector<std::string> parser(std::string str)
 {
+    std::vector<std::string> commandies;
+    std::string substr;
+    for(int i = 0; i < str.size(); i++)
+    {
+        
+        if(str.at(i) != ' ')
+        {
+            substr += str.at(i);
+        } else {
+            commandies.push_back(substr);
+            substr = "";
+        }
+    }
+    // there will be one more substring at the end of the line
+    commandies.push_back(substr);
 
+    return commandies;
+}
+
+// function to take the commands and execute them 
+void executer(std::vector<std::string> given_commands)
+{
+   int *array = new int[given_commands.size()]; // an array to represent the tree (in order)
+   // left child is at 2n right is at 2n+1
+   // if we correctly balance we should not go out of range
+    int index = 0;
+    for(auto command: given_commands){
+        // checking the first value
+        switch(command.at(0))
+        {
+            case 'A': // adding the values to the array
+                array[index] = std::stoi(command.substr(1,command.size()));
+                // std::cout << array[index] << "\n";
+                index++;
+
+                // restructuring if neccessary
+                break;
+
+            case 'D':
+            // std::cout << "del\n";
+                break;
+
+            default: // means we hit the post or pre or in order command (print)
+                break; 
+        }
+    } 
+    delete [] array;
 }
--- testing/testingparser.cpp
+++ testing/testingparser.cpp
@@ -2,13 +2,14 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> parser(std::string *);
+std::vector<std::string> parser(std::string str);
 
 int main()
 {
     std::string line;
-    std::cin >> line;
-    std::vector<std::string> commands = parser(&line);
+    // std::cin >> line;
+    std::getline(std::cin, line);
+    std::vector<std::string> commands = parser(line);
     for(auto a: commands){
         std::cout << a << "\n";
     }
@@ -18,18 +19,23 @@ int main()
 Takes a string filled with spaces and stores the parts into a vector
 Returns vector
 */
-std::vector<std::string> parser(std::string *str)
+std::vector<std::string> parser(std::string str)
 {
     std::vector<std::string> commandies;
     std::string substr;
-    for(int i = 0; i < str->size(); i++)
+    for(int i = 0; i < str.size(); i++)
     {
         
-        if(str->at(i) != ' ')
+        if(str.at(i) != ' ')
         {
-            substr += str->at(i);
+            substr += str.at(i);
         } else {
-            
+            commandies.push_back(substr);
+            substr = "";
         }
     }
+    // there will be one more substring at the end of the line
+    commandies.push_back(substr);
+
+    return commandies;
 }
