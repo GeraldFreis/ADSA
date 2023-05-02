ab9604a548b23532004ecf9743a1e1359e6ec09a
Gerald Freislich <freislichg@gmail.com>
2023-05-02 12:15:37 +0930

    safe

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ class Table {
         int findIndex(std::string val);
         void Insert(std::string val);
         void Delete(std::string val);
+        void printTable();
 };
 
 Table::Table()
@@ -23,6 +24,7 @@ Table::Table()
     // setting up each entry
     for(int i = 0; i < 26; i++){
         this->entries[i].key = 'a' + i;
+        this->entries[i].value = "Never Used";
     }
 }
 
@@ -34,24 +36,41 @@ int Table::findIndex(std::string val){
 }
 
 void Table::Insert(std::string val){
-
+    int corresponding_index = findIndex(val);
+    if(entries[corresponding_index].value == "Never Used"){ // if the index is never used
+        entries[corresponding_index].value = val;
+    }
 }
 void Table::Delete(std::string val){
+    int corresponding_index = findIndex(val);
+    if(entries[corresponding_index].value != "Never Used"){ // if the index is never used
+        entries[corresponding_index].value = "0";
+    }
+}
 
+void Table::printTable(){
+    for(int i = 0; i < 26; i++){
+        if(entries[i].value == "0" || entries[i].value == "Never Used"){
+            continue;
+        } else {
+            std::cout << entries[i].value << " ";
+        }
+    }
 }
 
 int main(){ // doing everything at the same time as we receive the input
-    std::string command;
-
-    while(std::cin >> command){
+    std::string command="";
+    Table tab;
+    while(command != "\n" && command != "\0"){
         // for each command we just do what we want
-
+        std::cin >> command;
         if(command.size() < 2) {return;} // base case
-        if(command.at(0) == 'A'){ // if we have an addition
 
+        if(command.at(0) == 'A'){ // if we have an addition
+            tab.Insert(command.substr(1, command.size()));
         }
         else if(command.at(0) == 'D'){ // if we have a deletion from the table
-
+            tab.Delete(command.substr(1, command.size()));
         }
 
     }
