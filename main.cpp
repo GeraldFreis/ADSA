dec412da00db9ca55068a29f8a28abcdaced4a23
Gerald Freislich <freislichg@gmail.com>
2023-05-02 12:02:58 +0930

    safe

--- main.cpp
+++ main.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 #include <string>
 
-struct Entry {
+struct Pair {
     char key;
     std::string value;
 };
 
 class Table {
     private:
-        Entry *entries;
-        
+        Pair *entries;
+    public:
+        Table();
+        ~Table();
+        int findIndex(std::string val);
+        void Insert(std::string val);
+        void Delete(std::string val);
 };
 
+Table::Table()
+{
+    this->entries = new Pair[26];
+    // setting up each entry
+    for(int i = 0; i < 26; i++){
+        this->entries[i].key = 'a' + i;
+    }
+}
 
+Table::~Table(){delete [] entries;}
+
+int Table::findIndex(std::string val){
+    // i know index will be at val.end - 'a' // because I'm smart
+    return int(val.at(val.size()-1)- 'a');
+}
+
+void Table::Insert(std::string val){
+
+}
+void Table::Delete(std::string val){
+
+}
 
 int main(){ // doing everything at the same time as we receive the input
     std::string command;
